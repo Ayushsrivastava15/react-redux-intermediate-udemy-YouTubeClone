1ae04bebaf7c994406b003f9040e0439ba10cf2f
Aaron Arinder <aaronarinder@gmail.com>
2017-09-14 14:19:51 -0400

    adding videolist

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+
 const API_KEY = ' AIzaSyCs_sQLyN30p_l-8_IPZGbEE1PhlszE8ZM ';
 
 class App extends Component {
@@ -18,6 +20,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
